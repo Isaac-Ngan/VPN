77e6b70149c314714b601de23d02147deacfbefa
Isaac-Ngan <78080344+Isaac-Ngan@users.noreply.github.com>
2025-05-21 20:15:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ fi
 ENCRYPT="chacha20-ietf-poly1305"
 QR_Path="/qr"
 
-#V2Ray Configuration
+# V2Ray Configuration
 V2_Path="/v2"
 mkdir /wwwroot
 mv /v2 /usr/bin/v2
@@ -15,20 +15,23 @@ if [ ! -d /etc/shadowsocks-libev ]; then
   mkdir /etc/shadowsocks-libev
 fi
 
-# TODO: bug when PASSWORD contain '/'
-sed -e "/^#/d"\
-    -e "s/\${PASSWORD}/${Password}/g"\
-    -e "s/\${ENCRYPT}/${ENCRYPT}/g"\
-    -e "s|\${V2_Path}|${V2_Path}|g"\
-    /conf/shadowsocks-libev_config.json >  /etc/shadowsocks-libev/config.json
-echo /etc/shadowsocks-libev/config.json
+# Generate config file without plugin lines (for debugging)
+sed -e "/^#/d" \
+    -e "s/\${PASSWORD}/${Password}/g" \
+    -e "s/\${ENCRYPT}/${ENCRYPT}/g" \
+    -e "s|\${V2_Path}|${V2_Path}|g" \
+    -e "/\"plugin\":/d" \
+    -e "/\"plugin_opts\":/d" \
+    /conf/shadowsocks-libev_config.json > /etc/shadowsocks-libev/config.json
+
+echo "/etc/shadowsocks-libev/config.json contents:"
 cat /etc/shadowsocks-libev/config.json
 
-sed -e "/^#/d"\
-    -e "s/\${PORT}/${PORT}/g"\
-    -e "s|\${V2_Path}|${V2_Path}|g"\
-    -e "s|\${QR_Path}|${QR_Path}|g"\
-    -e "$s"\
+sed -e "/^#/d" \
+    -e "s/\${PORT}/${PORT}/g" \
+    -e "s|\${V2_Path}|${V2_Path}|g" \
+    -e "s|\${QR_Path}|${QR_Path}|g" \
+    -e "$s" \
     /conf/nginx_ss.conf > /etc/nginx/conf.d/ss.conf 
 
 if [ "${Domain}" = "no" ]; then
@@ -40,9 +43,10 @@ else
   echo -n "${ss}" | qrencode -s 6 -o /wwwroot/vpn.png
 fi
 
-echo "Starting ss-server with command:"  
-echo "ss-server -c /etc/shadowsocks-libev/config.json &"  
-ss-server -c /etc/shadowsocks-libev/config.json &
+echo "Starting ss-server with explicit parameters:"
+echo "ss-server -s 0.0.0.0 -p 2333 -k ${Password} -m ${ENCRYPT} -u --plugin v2 --plugin-opts server;path=${V2_Path} &"
+
+ss-server -s 0.0.0.0 -p 2333 -k "${Password}" -m ${ENCRYPT} -u --plugin v2 --plugin-opts "server;path=${V2_Path}" &
 
 rm -rf /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
